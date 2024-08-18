64abb3e4902fc0b71c92aec8e644659c412b7b36
Kubirill <Kubirill@rambler.ru>
2024-08-18 13:54:09 +0300

    Fix CameraMove

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -14,12 +14,13 @@ public class CameraFollow : MonoBehaviour
     }
     private void Resize(Vector2Int direction, Vector2Int size)
     {
-        Vector3 dir = new Vector3(direction.x, direction.y, 0);
-        transform.position = transform.position+ dir/2;
-        if ((sizeMap.x<size.x)|| (sizeMap.y < size.y))
+        if ((sizeMap.x < size.x) || (sizeMap.y < size.y))
         {
-            _camera.orthographicSize = _camera.orthographicSize+LevelStats.sizeBlock/2;
+            _camera.orthographicSize = _camera.orthographicSize + LevelStats.sizeBlock / 2;
             sizeMap += Vector2Int.one;
         }
+        Vector3 dir = new Vector3(direction.x, direction.y, 0);
+        transform.position = transform.position+ dir * LevelStats.sizeBlock/2;
+
     }
 }
